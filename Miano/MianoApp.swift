cad326a4678356b2e5338b1e400ccc1248450601
Aayush9029 <aayushpokharel36@gmail.com>
2023-05-30 00:57:16 -0400

    Initial Commit

---
+++ Miano/MianoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MianoApp.swift
+//  Miano
+//
+//  Created by Aayush Pokharel on 2023-05-30.
+//
+
+import SwiftUI
+
+@main
+struct MianoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
